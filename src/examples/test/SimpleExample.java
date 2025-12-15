569727c34ad9f145082f7e1e13250b3727fe83a8
SonnyZ789123 <yoran.mertens@telenet.be>
2025-12-15 15:41:27 +0100

    Extend examples

--- src/examples/test/SimpleExample.java
+++ src/examples/test/SimpleExample.java
@@ -2,14 +2,29 @@ package test;
 
 public class SimpleExample {
 
-    // The function JDart will explore symbolically
+    public static int bar(int d) {
+        System.err.println("\n-------- In bar! Parameter = " + d);
+        if (d >= 3) {
+            System.err.printf("%s\n", "d <= 10 && d >= 3");
+            return -10;
+        } else {
+            System.err.printf("%s\n", "d <= 10 && d < 3");
+            return 10;
+        }
+    }
+
     public static int foo(int x) {
+        System.err.println("\n-------- In foo! Parameter = " + x);
         if (x == 0) {
+            System.err.printf("%s\n", "x == 0");
             return 0;
         } else if (x > 10) {
+            System.err.printf("%s\n", "x > 10");
             return 1;
         } else {
-            return -1;
+            System.err.printf("%s\n", "x <= 10");
+            int result = bar(x);
+            return result;
         }
     }
 
