270ddf0613517242838989b69def9595c8af5e7b
SonnyZ789123 <yoran.mertens@telenet.be>
2026-01-11 08:09:13 +0100

    Add testsuites test

---
+++ src/examples/test/testsuites/Test.java
@@ -0,0 +1,37 @@
+package test.testsuites;
+
+public class Test {
+
+    public int bar(int a) {
+        if (a == 4) {
+            return 4;
+        }
+        if (a == 5) {
+            return 5;
+        }
+        if (a== 6) {
+            return 6;
+        }
+        return a + 100;
+    }
+
+    public static int foo(int x, int y) {
+        if (x == 1) {
+            return 1;
+        }
+        if (x == 2) {
+            return 2;
+        }
+        if (x== 3) {
+            return 3;
+        }
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        foo(1, 2);
+
+        Test test = new Test();
+        test.bar(4);
+    }
+}
